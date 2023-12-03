3e4445fb174e9604e93ba8282c0b743aa4609163
Kuldeep Nageshwar <35416870+kuldeepNageshwar17@users.noreply.github.com>
2023-12-03 15:26:54 +0530

    layout-components with splot scree

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { SplitScreen } from "./SplitScreen";
+import "./App.css";
 
+const LeftHandComponent = ({ name }) => {
+  return <h1>{name}</h1>;
+};
+const RightHandComponent = ({ message }) => {
+  return <h1>{message}</h1>;
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SplitScreen LeftWeight={1} RightWeight={3}>
+      <LeftHandComponent name={"shawn"} />
+      <RightHandComponent message={"How are you shawn"} />
+    </SplitScreen>
   );
 }
 
---
+++ src/SplitScreen.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+const Container = styled.div`
+  display: flex;
+`;
+const Pane = styled.div`
+  flex: ${(props) => props.weight};
+`;
+
+export const SplitScreen = ({ children, LeftWeight = 1, RightWeight = 1 }) => {
+  const [left, right] = children;
+  return (
+    <Container>
+      <Pane weight={LeftWeight}>{left}</Pane>
+      <Pane weight={RightWeight}>{right}</Pane>
+    </Container>
+  );
+};
